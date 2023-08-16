7361046a40b8eb7de32f231ada17f4883d0055b3
darlanZero <105607085+darlanZero@users.noreply.github.com>
2023-08-16 09:15:06 -0300

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com', '192.168.100.97'],
   },
+  future: {
+    webpack5: true,  
+  }, 
 }
 
 module.exports = nextConfig
